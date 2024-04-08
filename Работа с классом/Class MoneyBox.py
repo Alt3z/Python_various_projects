10757500932a51a074debf495d39bde5a20ffaee
Фёдор Онищенко <166149674+Alt3z@users.noreply.github.com>
2024-04-08 23:39:39 +0500

    Update Class MoneyBox.py

--- Работа с классом/Class MoneyBox.py
+++ Работа с классом/Class MoneyBox.py
@@ -1,1 +1,10 @@
+class MoneyBox:
+    def __init__(self, capacity): # конструктор с аргументом – вместимость копилки
+        self.count = 0
+        self.capacity = capacity
 
+    def can_add(self, v):  # True, если можно добавить v монет, False иначе
+        return (self.count + v) <= self.capacity
+
+    def add(self, v): # положить v монет в копилку
+        self.count += v
